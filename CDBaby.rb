8ad298780e1a4efb8e88f8a125afe7e34a2d8e70
Mashychev <teplii@30.cm>
2016-02-22 18:03:03 +0200

    Improved_code

--- CDBaby.rb
+++ CDBaby.rb
@@ -2,30 +2,21 @@ require 'nokogiri'
 require 'digest/md5'
 
 def cd_baby_batch_parser
-	route_to_xmls = Dir.glob('./**/*.xml')
-	xmlfeed = []
-	route_to_xmls.each do |route|
-		xmlfeed << Nokogiri::XML(open(route))
-	end
-	hash_sums = []
-	xmlfeed.each do |feed|
-		items = feed.xpath("//HashSum/HashSum")
-		items.each do |item|
-			hash_sums << item.children.text
+	Dir.glob('./**/*.xml').map do |route| 
+		Nokogiri::XML(open(route))
+	end.each_with_object([]) do |feed, hash_sums|
+		feed.xpath("//HashSum/HashSum").each do |item|
+			hash_sums << item.children.text.upcase
 		end
 	end
-	hash_sums
 end
 
 def hash_sum_for_batch_files
-	route_to_files = Dir.glob('./**/*.{flac,jpg}')
-	files_hash_sums = []
-	route_to_files.each do |route|
-		files_hash_sums << Digest::MD5.file(route).to_s.upcase 
+	Dir.glob('./**/*.{flac,jpg,mp3,mp4}').map do |route|
+		Digest::MD5.file(route).to_s.upcase 
 	end
-	files_hash_sums
 end
 
-def delivered_method(cd_baby_batch_parser, hash_sum_for_batch_files)
+def delivered_method
 	puts 'All Batches are delivered' if (cd_baby_batch_parser - hash_sum_for_batch_files).empty?
 end
